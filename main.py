b33b3bb10e7b279573d6acaeac6437fff7d110ea
Sven <denhartog.sven@gmail.com>
2018-04-21 16:00:00 +0200

    More feedback for server

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from utils import butter_bandpass_filter, get_timit_dict, get_target, create_mel
 from python_speech_features import mfcc
 import os
 
-dirlist = "./data/"
-labels = get_timit_dict("phonedict.txt")
+dirlist = './data/'
+labels = get_timit_dict('phonedict.txt')
 
 # preprocess data
 spectograms = []
 mfccs = []
 phones = []
 for d in dirlist:
-    for dirName, subdirList, fileList in os.walk("./data/TRAIN/"):
+    for dirName, subdirList, fileList in os.walk('./data/TRAIN/'):
         for fname in fileList:
-            if not fname.endswith('.PHN') or (fname.startswith("SA")):
+            if not fname.endswith('.PHN') or (fname.startswith('SA')):
                 continue
 
             phn_fname = dirName + fname
@@ -27,5 +27,7 @@ for d in dirlist:
             spectograms.append(pretty_spectrogram(data.astype('float64')))
             mfccs.append(mfcc(data, rate))
             phones.append(get_target(phn_fname, labels, data.shape[0]))
+
+            print('Loaded: {}'.format(fname[0:-4]))
 
 # TODO: build and train model
