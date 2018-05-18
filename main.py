cff38a901fb0af2d7e472aa425f300c51bb70228
Denise <denise.klep@student.ru.nl>
2018-05-18 21:04:19 +0200

    test set + test loop toegevoegd, epochs = 5

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from sklearn.metrics import accuracy_score
 labels = get_timit_dict('phonedict.txt')
 nr_classes = len(labels)
 batch_size = 64
-epochs = 1
+epochs = 5
 
 rate = 16000 #16000fps - 0.0625ms per frame
 stepsize = 64 #for spectogram reduction
@@ -23,8 +23,11 @@ frame_step = (int)((0.010 * rate) / stepsize) #15ms
 print('Frame size: {}, frame step size: {}'.format(frame_size, frame_step))
 
 # preprocess data
-dataset = TimitDataset('./data', labels, stepsize, freq_bins, frame_step, frame_size)
-dataloader = get_batch_data(dataset, batch_size)
+training_set = TimitDataset('./data', labels, stepsize, freq_bins, frame_step, frame_size)
+trainloader = get_batch_data(training_set, batch_size)
+
+test_set = TimitDataset('./data', labels, stepsize, freq_bins, frame_step, frame_size, traintest='TEST')
+testloader = get_batch_data(training_set, batch_size)
 
 device = torch.cuda.device(0)
 
@@ -40,11 +43,11 @@ def train_model(model, optimizer, num_epochs=10):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
         model.train()
+        running_loss = 0.0
 
-        for inputs, labels in dataloader:
+        for idx, (inputs, labels) in enumerate(trainloader, 0):
 
             inputs = Variable(inputs).cuda()
-            #print(inputs.size())
             labels = Variable(labels).cuda()
 
             optimizer.zero_grad()
@@ -57,11 +60,36 @@ def train_model(model, optimizer, num_epochs=10):
             loss.backward()
             optimizer.step()
 
-            print('Loss: {:.4f}'.format(
-                (float)(loss.data)))
+            # print statistics
+            running_loss += loss.item()
+            if idx % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, idx + 1, running_loss / 10))
+                running_loss = 0.0
+
+            #print('Loss: {:.4f}'.format(
+
+            #    (float)(loss.data)))
 
         print()
 
     return model
 
+def test_model(model):
+
+    correct = 0.0
+    total = 0.0
+
+    for inputs, labels in testloader:
+
+        outputs, reconstructions = model(inputs)
+        _, preds = torch.max(outputs, 1)
+
+        total += labels.size(0)
+        correct += (preds == labels).sum().item()
+
+    print('Accuracy of the network on the test images: %d %%' % (
+        100 * correct / total))
+
 model = train_model(model, optimizer, num_epochs=epochs)
+test_model(model)
