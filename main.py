f51d23215bc72df52448153be94d53217a83a500
Sven <denhartog.sven@gmail.com>
2018-05-20 13:55:00 +0200

    print fewer loss measures for convnet

--- main.py
+++ main.py
@@ -77,18 +77,12 @@ def train_model(model, optimizer, num_epochs=10):
             loss.backward()
             optimizer.step()
 
-            if model == capsnet:
-                # print statistics
-                running_loss += loss.item()
-                if idx % 2000 == 1999:  # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, idx + 1, running_loss / 2000))
-                    running_loss = 0.0
-
-            else:
-                print('Loss: {:.4f}'.format(
-
-                    (float)(loss.data)))
+            # print statistics
+            running_loss += loss.item()
+            if idx % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, idx + 1, running_loss / 2000))
+                running_loss = 0.0
 
     return model
 
